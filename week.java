d83644a72ca54b95f0669949571bf64226a1470b
Ram734 <prasannaking48@gmail.com>
2017-06-06 23:10:35 +0530

    Add files via upload

---
+++ week.java
@@ -0,0 +1,20 @@
+class week
+{
+public static void main(String args[])
+{
+String s=args[0];
+if(s== "sunday")
+{
+	System.out.print("Leave");
+}
+else if(s=="saturday")
+{
+	System.out.print("Leave");
+}
+
+else
+{
+	System.out.print("week days");
+}
+}
+}
