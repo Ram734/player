e89fac9c4717f8bc57bd0d5421e188adaacd2d93
Ram734 <prasannaking48@gmail.com>
2017-06-08 09:42:21 +0530

    Add files via upload

---
+++ happy.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class happy
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int f=sc.nextInt();
+        while(f!=1)
+        {
+            int a=0;
+        while(f>0)
+        {
+            int g=0;
+            g=f%10;
+            a+=(g*g);
+            f=f/10;
+        }
+            if(a==1)
+            {
+                System.out.print("True");
+                System.exit(0);
+            }
+            else
+            {
+              f=a;
+            }
+        }
+            
+        
+        
+            
+    } 
+}
+
+
+
